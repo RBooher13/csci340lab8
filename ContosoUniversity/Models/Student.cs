0a20052ec3fa1983c12d558a7c8aeca2e4722e11
RhettBooher13 <rhettbooher@gmail.com>
2021-10-25 11:49:39 -0500

    Scaffolded Student pages and updated database context.

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace ContosoUniversity.Models
 {
-    public class Students
+    public class Student
     {
-        public Students()
-        {
-        }
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public int Age { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
